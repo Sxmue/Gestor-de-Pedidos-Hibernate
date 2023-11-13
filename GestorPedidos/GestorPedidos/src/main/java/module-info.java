f54642e2052cefcfeb8fb6cec1f91a349b6e79d2
Sxmue <116808980+Sxmue@users.noreply.github.com>
2023-11-13 19:04:10 +0100

    Delete GestorPedidos directory

--- GestorPedidos/GestorPedidos/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.cesur.gestortareas {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.cesur.gestorpedidos to javafx.fxml;
-    exports com.cesur.gestorpedidos;
-}
